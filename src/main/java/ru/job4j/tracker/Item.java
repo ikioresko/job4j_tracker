cb81c45f8f2fa77fd6052c99e0a433c3617a1c6a
ikioresko <ikioresko@gmail.com>
2020-12-08 11:31:46 +0500

    7. Date. Отображение даты. [#418324]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -15,6 +18,8 @@ public class Item {
         this.id = id;
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {return created;}
 
     public int getId() {
         return id;
