322e32e8e7dc68871e107d8be453e37aa75f6796
ikioresko <ikioresko@gmail.com>
2020-12-31 18:33:32 +0500

    0. Сортировка [#418234]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
     private int id;
@@ -48,5 +49,18 @@ public class Item {
                 ", name = '" + name + '\'' +
                 ", created = " + created +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -62,5 +67,35 @@ public class TrackerTest {
         int id = bug.getId();
         tracker.delete(id);
         assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void whenSortFromBigToSmall() {
+        List<Item> expected = new ArrayList<Item>();
+        expected.add (new Item(3, "test3"));
+        expected.add (new Item(2,"test2"));
+        expected.add (new Item(1,"test1"));
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("test1"));
+        tracker.add(new Item("test2"));
+        tracker.add(new Item("test3"));
+        List<Item> original = tracker.findAll();
+        Collections.sort(original, new SortToSmall());
+        assertThat(expected, is(original));
+    }
+
+    @Test
+    public void whenSortFromSmallToBig() {
+        List<Item> sorted = new ArrayList<Item>();
+        sorted.add (new Item(3, "test3"));
+        sorted.add (new Item(2,"test2"));
+        sorted.add (new Item(1,"test1"));
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("test1"));
+        tracker.add(new Item("test2"));
+        tracker.add(new Item("test3"));
+        List<Item> original = tracker.findAll();
+        Collections.sort(sorted, new SortToBig());
+        assertThat(sorted, is(original));
     }
 }
