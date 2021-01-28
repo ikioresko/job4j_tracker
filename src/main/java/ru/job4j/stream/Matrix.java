056e2c6df1172a884dc2efdc7d88efc7185f1f47
ikioresko <ikioresko@gmail.com>
2021-01-28 14:48:43 +0500

    5. Преобразование матрицы чисел в список чисел. [#418073]

--- src/main/java/ru/job4j/stream/Matrix.java
+++ src/main/java/ru/job4j/stream/Matrix.java
@@ -1,12 +1,14 @@
 package ru.job4j.stream;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Matrix {
-    public static void main(String[] args) {
-        Integer[][] matrix = {{1, 2, 3}, {4, 5, 6}};
-        System.out.println(Stream.of(matrix).flatMap(Arrays::stream).collect(Collectors.toList()));
+    public List<Integer> sort(Integer[][] array) {
+        return Stream.of(array)
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/test/java/ru/job4j/stream/MatrixTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void testSort() {
+        Integer[][] array = {{1, 2, 3}, {4, 5, 6}};
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Matrix m = new Matrix();
+        List<Integer> actual = m.sort(array);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testSortSecond() {
+        Integer[][] array = {{1, 2,}, {3, 4}, {5, 6}};
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Matrix m = new Matrix();
+        List<Integer> actual = m.sort(array);
+        assertThat(actual, is(expected));
+    }
+}
