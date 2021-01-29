2dc38cc6c488ed431aee0eb8e1e94837458a4e48
ikioresko <ikioresko@gmail.com>
2021-01-29 11:05:19 +0500

    9. Шаблоны проектирования в Stream API [#418077]

---
+++ src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,83 @@
+package ru.job4j.stream;
+
+public class Car {
+    String model;
+    String color;
+    double engine;
+    double fuelTank;
+    int door;
+    int wheelsInch;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                ", fuelTank=" + fuelTank +
+                ", door=" + door +
+                ", wheelsInch=" + wheelsInch +
+                '}';
+    }
+
+    static class Builder {
+        String model;
+        String color;
+        double engine;
+        double fuelTank;
+        int door;
+        int wheelsInch;
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildEngine(double engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Builder buildFuelTank(double fuelTank) {
+            this.fuelTank = fuelTank;
+            return this;
+        }
+
+        Builder buildDoor(int door) {
+            this.door = door;
+            return this;
+        }
+
+        Builder buildWheelsInch(int wheelsInch) {
+            this.wheelsInch = wheelsInch;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.model = model;
+            car.color = color;
+            car.engine = engine;
+            car.fuelTank = fuelTank;
+            car.door = door;
+            car.wheelsInch = wheelsInch;
+            return car;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder().buildModel("Lada")
+                .buildColor("White")
+                .buildEngine(1.6)
+                .buildFuelTank(45)
+                .buildDoor(4)
+                .buildWheelsInch(17)
+                .build();
+        System.out.println(car);
+    }
+}
