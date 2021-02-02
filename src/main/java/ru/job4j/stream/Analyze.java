c73fe41030e7078b5a2d2b2bbb1ab2095c00924e
ikioresko <ikioresko@gmail.com>
2021-02-02 21:20:44 +0500

    2. Аттестация [#418086]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,78 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(c -> c.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(s -> new Tuple(s.getName(), s.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return (stream
+                .flatMap(s -> s.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName
+                        , Collectors.averagingDouble(Subject::getScore))))
+                .entrySet()
+                .stream()
+                .map(t -> new Tuple(t.getKey(), t.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(s -> new Tuple(s.getName()
+                , s.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore)
+                .sum()))
+                .max((a, b) -> (int) (a.getScore() - (b.getScore())))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return (stream
+                .flatMap(s -> s.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName
+                        , Collectors.summingDouble(Subject::getScore))))
+                .entrySet()
+                .stream()
+                .map(t -> new Tuple(t.getKey(), t.getValue()))
+                .max((a, b) -> (int) (a.getScore() - (b.getScore())))
+                .orElse(null);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
