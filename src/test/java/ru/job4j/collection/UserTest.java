e8a1694634c9f9aad5a5b94debf8fffbc737d1bf
ikioresko <ikioresko@gmail.com>
2021-04-04 20:28:51 +0500

    4.5. Travis CI и PostgreSQL [#467239]

--- src/test/java/ru/job4j/collection/UserTest.java
+++ src/test/java/ru/job4j/collection/UserTest.java
@@ -3,8 +3,7 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.List;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
@@ -13,10 +12,11 @@ import static org.junit.Assert.*;
 public class UserTest {
     @Test
     public void whenAsc() {
-        Set<User> users = Set.of(new User("Petr", 32), new User("Ivan", 31));
+        List<User> users = List.of(new User("Petr", 32), new User("Ivan", 31));
         Iterator<User> it = users.iterator();
-        assertThat(it.next(), is(new User("Ivan", 31)));
         assertThat(it.next(), is(new User("Petr", 32)));
+        assertThat(it.next(), is(new User("Ivan", 31)));
+
     }
 
     @Test
